024c038d46fa55a939b40eb70e460fa7eb370595
Swastik Biswas <swastikbiswas962@gmail.com>
2022-11-17 20:41:28 +0530

    personal data api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({
+    name: 'Swastik Biswas',
+    internet_name: 'Polybit',
+    projects: [
+      {
+        name: 'Coffee IDE for Java Developers',
+        version: '0.1.1'
+      }
+    ]
+  })
 }
