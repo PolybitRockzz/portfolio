4638c245fac9fd35a7b37ba670fb320954531b89
Swastik Biswas <swastikbiswas962@gmail.com>
2022-11-23 19:40:37 +0530

    updated configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const { IMAGES_MANIFEST } = require('next/dist/shared/lib/constants')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: './',
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       <Head>
         <title>Home | Swastik Biswas</title>
         <meta name="description" content="swastik polybit biswas, portfolio, projects, contact" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="../public/favicon.ico" />
       </Head>
       
       <div className={styles.banner}>
