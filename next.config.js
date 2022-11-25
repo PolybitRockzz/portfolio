ced8a3c88102f4f705efd489fd5f9bbdaed7b56c
Swastik Biswas <swastikbiswas962@gmail.com>
2022-11-25 17:28:47 +0530

    updated nextjs config

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const { IMAGES_MANIFEST } = require('next/dist/shared/lib/constants')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: './',
 }
 
 module.exports = nextConfig
