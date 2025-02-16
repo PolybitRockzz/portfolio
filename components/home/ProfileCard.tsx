e7cddc24976bbf4ceded4d9ffe463fe3197c6bec
Swastik Biswas <swastikbiswas962@gmail.com>
2025-02-17 01:56:06 +0530

    incredibly small change of truncating West Bengal to WB (wtf)

--- components/home/ProfileCard.tsx
+++ components/home/ProfileCard.tsx
@@ -8,7 +8,7 @@ const ProfileCard = () => {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 15" fill="none" className="w-4 h-4">
           <path d="M6 3.5C6 2.67157 6.67157 2 7.5 2S9 2.67157 9 3.5 8.32843 5 7.5 5C6.67157 5 6 4.32843 6 3.5zM8 5.94999C9.14112 5.71836 10 4.70948 10 3.5 10 2.11929 8.88071 1 7.5 1 6.11929 1 5 2.11929 5 3.5c0 1.20948.85888 2.21836 2 2.44999V13.5c0 .2761.22386.5.5.5s.5-.2239.5-.5V5.94999z" fill={"currentcolor"} fillRule={"evenodd"} clipRule={"evenodd"} />
         </svg>
-        Kolkata, West Bengal 🇮🇳  |  Wilmington, DE 🇺🇸
+        Kolkata, WB 🇮🇳  |  Wilmington, DE 🇺🇸
       </p>
       <p className="mt-3 text-neutral-300">
         An 18 year old software engineer, developer. Residing in India, citizen of the United States. Proficient in desktop applications, web applications, and machine learning models. Focusing on user experience and performance. Currently pursuing a B.Tech in Computer Science and Engineering at KIIT University.
